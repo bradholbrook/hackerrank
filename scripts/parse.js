a0283171186fe9bea634453ac05f22dd2c69f3de
Eric Hotinger <ehotinger@gmail.com>
2013-11-18 09:14:55 -0500

    Nothing really done yet - just adding Facebook Oauth for the future and the "test" for Parse to store data.  Placeholder commit.

---
+++ scripts/parse.js
@@ -0,0 +1,10 @@
+﻿Parse.initialize("1aDfs1rC94XPfqGxm9uKil8nOLHuLGMUHgmKaGcf", "ehlesfUmOQ6OxyjiyhOA9pzB6Bu12aOycq1qMeyU");
+
+
+var TestObject = Parse.Object.extend("TestObject");
+var testObject = new TestObject();
+testObject.save({ foo: "bar" }, {
+    success: function (object) {
+        alert("yay! it worked");
+    }
+});
