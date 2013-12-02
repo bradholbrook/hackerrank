58199df4b954d8e9fd7d49e4f0258c4b86afde97
Bradley Holbrook <holbrookbw1@gmail.com>
2013-12-02 13:17:22 -0500

    Initial commit, gitignore

---
+++ HackerRank/Pairs/Pairs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pairs
+{
+    class Pairs
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
